ea98d9dc3192661fdce5a84afe62ce1b8fc567f3
AyaShiau <63468278+adbshiau@users.noreply.github.com>
2023-09-05 16:04:51 -0700

    register user function created

--- schedule_app/views.py
+++ schedule_app/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 
 
 def home(request):
     return render(request, 'base.html')
+
+
+def returnRegisterPage(request):
+    form = UserCreationForm()
+    if request.method == 'POST':
+        form - UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+    context = {'form': form}
+    return render(request, 'register.html', context)
