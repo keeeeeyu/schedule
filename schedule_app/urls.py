892f90a4522016bb96133293e261161803a87fe4
AyaShiau <63468278+adbshiau@users.noreply.github.com>
2023-09-09 10:17:04 -0700

    sign up is working - no first name and last name

--- schedule_app/urls.py
+++ schedule_app/urls.py
@@ -3,6 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('register', views.returnRegisterPage, name='register'),
     path('signup', views.signup, name='signup'),
 ]
--- schedule_app/views.py
+++ schedule_app/views.py
@@ -2,21 +2,23 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+from .forms import CreateUserForm
 
 
 def home(request):
     return render(request, 'base.html')
 
 
-def returnRegisterPage(request):
-    form = UserCreationForm()
-    if request.method == 'POST':
-        form - UserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-    context = {'form': form}
-    return render(request, 'register.html', context)
-
-
 def signup(request):
-    return render(request, 'registration/signup.html')
+    form = CreateUserForm()
+
+    if request.method == 'POST':
+        print("method is post")
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            print("form is valid")
+            form.save()
+        else:
+            print("form isnt valid")
+    context = {'form': form}
+    return render(request, 'registration/signup.html', context)
