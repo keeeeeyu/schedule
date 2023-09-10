108d89e9a72d831c18a8db53164760c2a411cfc9
AyaShiau <63468278+adbshiau@users.noreply.github.com>
2023-09-09 17:25:56 -0700

    sign up is working

--- schedule_app/urls.py
+++ schedule_app/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.loginPage, name='login'),
     path('signup', views.signup, name='signup'),
+    path('home', views.home, name='home'),
 ]
--- schedule_app/views.py
+++ schedule_app/views.py
@@ -1,22 +1,42 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 from .forms import CreateUserForm
 
 
 def home(request):
-    return render(request, 'base.html')
+    return render(request, 'home.html')
+
+
+def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.info(request, 'Username OR Password is incorrect')
+
+    context = {}
+    return render(request, 'login.html', context)
 
 
 def signup(request):
     form = CreateUserForm()
 
     if request.method == 'POST':
-        print("method is post")
         form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, "Account was created for " + user)
             return redirect('home')
     context = {'form': form}
     return render(request, 'registration/signup.html', context)
