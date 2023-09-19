8dfd65b45db2c705f17509202b6c4d7758943031
Kee Yu <keeeeeyu@gmail.com>
2023-09-19 02:43:15 -0700

    clock in and out added to timesheets. Now working on getting hours worked

--- schedule_app/views.py
+++ schedule_app/views.py
@@ -5,7 +5,6 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from .forms import CreateUserForm
 from django.utils import timezone
-from django.utils import timezone
 from .models import User_worktime
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
@@ -63,16 +62,25 @@ def timesheets(request):
 @login_required
 def clock_in(request):
     if request.method == 'POST':
+        first_entry = User_worktime.objects.filter(user=request.user).first()
         last_entry = User_worktime.objects.filter(user=request.user).last()
-        print('last entry', last_entry)
-        if last_entry.clock_in and last_entry.clock_out is not None:
+        print('last entry', first_entry)
+        if first_entry is None:
             user = request.user
             clock_in_time = timezone.now()
             clocked_in = User_worktime.objects.create(
                 user=user, clock_in=clock_in_time)
             clocked_in.save()
             messages.success(
-                request, f'Clock-in ({clock_in_time}) successful.')
+                request, f'Clock-in ({timezone.localtime(clock_in_time).strftime("%Y-%m-%d %H:%M:%S")}) successful.') 
+        elif last_entry.clock_in and last_entry.clock_out is not None:
+            user = request.user
+            clock_in_time = timezone.now()
+            clocked_in = User_worktime.objects.create(
+                user=user, clock_in=clock_in_time)
+            clocked_in.save()
+            messages.success(
+                request, f'Clock-in ({timezone.localtime(clock_in_time).strftime("%Y-%m-%d %H:%M:%S")}) successful.')
         else:
             messages.error(request, 'You are already clocked in.')
     return render(request, 'home.html')
@@ -87,8 +95,30 @@ def clock_out(request):
             last_entry.clock_out = clock_out_time
             last_entry.save()
             messages.success(
-                request, f'Clock-out ({clock_out_time}) successful.')
-            messages.error(request, 'Error')
+                request, f'Clock-out ({timezone.localtime(clock_out_time).strftime("%Y-%m-%d %H:%M:%S")}) successful.')
         else:
             messages.error(request, 'You are already clocked out.')
     return render(request, 'home.html')
+
+@login_required
+def timesheets(request):
+    first_name = request.user.first_name
+    last_name = request.user.last_name
+    clock_ins = User_worktime.objects.values_list('clock_in', flat=True)
+    clock_outs = User_worktime.objects.values_list('clock_out', flat=True)
+    work_hours = []
+    for clock_in, clock_out in zip(clock_ins, clock_outs):
+        if clock_in and clock_out:
+            # Calculate the time difference between clock-in and clock-out
+            time_worked = clock_out - clock_in
+
+            # Extract the hours and minutes worked
+            hours_worked = divmod(time_worked.total_seconds(), 3600)
+
+            # Append the formatted work hours to the list
+            work_hours.append(hours_worked)
+        else:
+            work_hours.append('N/A')  # No clock-in or clock-out time
+            
+        context = {'first_name': first_name, 'last_name': last_name, 'clock_ins': clock_ins, 'clock_outs': clock_outs, 'work_hours': work_hours}
+    return render(request, 'account/timesheets.html', context)
