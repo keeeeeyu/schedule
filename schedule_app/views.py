7dce74cba3487a2c90e9bb86bbeb3b10656abf9e
AyaShiau <63468278+adbshiau@users.noreply.github.com>
2023-11-10 10:08:02 -0800

    add shift modal works

--- schedule_app/views.py
+++ schedule_app/views.py
@@ -237,7 +237,7 @@ def create_shift(request):
         'departments': departments
     }
 
-    return render(request, 'schedule/create.html', context)
+    return render(request, 'schedule.html', context)
 
 
 def add_shift(request):
